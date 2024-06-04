3af8c829ad9812cdb9d0cd12aec94acaeb5ec121
diyorbekravshanqulov <diyorbekravshanqulov17@gmail.com>
2024-06-04 20:53:29 +0500

    complate

---
+++ src/components/AboutBottom.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+const images = ref(["/simon.svg", "/paul.svg", "/simon.svg" , "/sara.svg"])
+const names = ref(["John Smith", "Simon Adams", "Paul Jones", "Sara Hardin"])
+const fields = ref(["CEO", "CTO", "Design Lead", "Project Manager"])
+
+</script>
+<template>
+  <div class="w-full px-[3%] py-[128px] text-blackBlue">
+    <h2 class="text-center leading-[64px] text-5xl font-semibold">Meet our team</h2>
+    <div class="flex mt-12 w-full justify-between">
+        <div class="py-12 px-16" v-for="(item, index) in images" :key="index">
+            <img class="rounded-full" :src="item" alt="">
+            <h6 class="mt-[30px] text-center text-2xl font-medium">{{ names[index] }}</h6>
+            <p class="mt-1 text-center">{{ fields[index] }}</p>
+        </div>
+    </div>
+  </div>
+</template>
--- src/pages/about_us.vue
+++ src/pages/about_us.vue
@@ -3,12 +3,14 @@ import Navbar from "../components/Navbar.vue";
 import Footer from "../components/Footer.vue";
 import AboutTop from "../components/AboutTop.vue";
 import AboutMiddle from "../components/AboutMiddle.vue";
+import AboutBottom from "../components/AboutBottom.vue";
 </script>
 <template>
   <div>
     <Navbar />
     <AboutTop />
     <AboutMiddle />
+    <AboutBottom />
     <Footer />
   </div>
 </template>
