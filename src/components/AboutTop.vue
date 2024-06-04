7c417416641a0ce59970da8611589dc79a2fb936
diyorbekravshanqulov <diyorbekravshanqulov17@gmail.com>
2024-06-04 20:36:42 +0500

    complate

--- src/components/AboutTop.vue
+++ src/components/AboutTop.vue
@@ -1,6 +1,6 @@
 <script setup></script>
 <template>
-  <div class="flex items-center w-full justify-between px-[3%] gap-[13%] py-[128px]">
+  <div class="flex items-center w-full justify-between px-[3%] gap-[13%] pt-[220px]">
     <div>
       <p class="text-blackBlue text-[18px] ">About us</p>
       <h1 class="mt-2  text-blackBlue text-[54px] leading-[74px] font-semibold">Our designs solve problems</h1>
