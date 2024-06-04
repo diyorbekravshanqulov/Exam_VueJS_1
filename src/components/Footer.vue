adee269c011252ba0485bc5c0536bc7c00b51f92
diyorbekravshanqulov <diyorbekravshanqulov17@gmail.com>
2024-06-04 09:57:17 +0500

    complate

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,20 +8,26 @@ const images = ref([
   "/linkdIn.svg",
 ]);
 
-
-
-
+const getLink = (index) => {
+  if (index === 0) {
+    return 'https://facebook.com';
+  } else if (index === 1) {
+    return 'https://twitter.com';
+  } else if (index === 2) {
+    return 'https://instagram.com';
+  } else {
+    return 'https://linkedin.com';
+  }
+};
 </script>
+
 <template>
-  <div
-    class="flex justify-start flex-wrap px-[3%] mt-[57px] py-[96px] bg-[#1C1E53]"
-  >
+  <div class="flex justify-start flex-wrap px-[3%] mt-[57px] py-[96px] bg-[#1C1E53]">
     <div class="flex gap-[260px]">
       <div>
-        <img src="../../public/logo.svg" alt="" />
+        <img src="../../public/logo.svg" alt="Company Logo" />
         <p class="mt-[23px] text-white leading-[28px] font-medium">
-          We are always open to discuss your project and improve your online
-          presence.
+          We are always open to discuss your project and improve your online presence.
         </p>
       </div>
       <div>
@@ -29,19 +35,16 @@ const images = ref([
           Lets Talk!
         </h2>
         <p class="mt-[14px] text-[#F4F6FC] leading-[28px] mb-7">
-          We are always open to discuss your project, improve your online
-          presence and help with your UX/UI design challenges.
+          We are always open to discuss your project, improve your online presence and help with your UX/UI design challenges.
         </p>
         <div class="flex gap-[27px] items-center">
-          <a href="#" v-for="(item, index) in images">
-            <img :key="index" :src="item" alt="logo_social" />
+          <a :href="getLink(index)" v-for="(item, index) in images" :key="index" target="_blank">
+            <img :src="item" alt="logo_social" />
           </a>
         </div>
       </div>
     </div>
-    <div
-      class="py-4 pr-[137px] pl-[37px] flex items-center gap-[29px] bg-[#FCD980] text-[#282938]"
-    >
+    <div class="py-4 pr-[137px] pl-[37px] flex items-center gap-[29px] bg-[#FCD980] text-[#282938]">
       <div>
         <h5 class="leading-[32px] text-[18px] font-medium">Email me at</h5>
         <p class="mt-1 leading-[28px]">contact@website.com</p>
