5cdfaf2ce08e88cc20c365d862e1e018f894c0c0
diyorbekravshanqulov <diyorbekravshanqulov17@gmail.com>
2024-06-05 14:12:22 +0500

    complate

--- src/components/Clients.vue
+++ src/components/Clients.vue
@@ -5,18 +5,18 @@
   <div class="container  max-md:px-[5%] grid md:grid-cols-3 gap-[10%] md:mt-[57px] md:py-[128px] py-[64px]">
     <div class="md:col-span-1 col-span-2">
       <h3
-        class="text-[38px] md:text-start text-center font-semibold leading-[56px]"
+        class="md:text-[38px] text-2xl max-md:px-8 md:text-start text-center font-semibold leading-[56px]"
         style="color: rgba(40, 41, 56, 1)"
       >
         What our clients say about us
       </h3>
-      <p class="md:mt-4 mt-8" style="color: rgba(40, 41, 56, 1)">
+      <p class="md:mt-4 max-md:text-[14px] mt-8" style="color: rgba(40, 41, 56, 1)">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit sed.
       </p>
     </div>
     <div class="col-span-2">
       <h5
-        class="md:text-[32px] text-[24px] font-medium md:leading-[48px] leading-[36px]"
+        class="md:text-[32px] text-[20px] font-medium md:leading-[48px] leading-[36px]"
         style="color: rgba(40, 41, 56, 1)"
       >
         "The best agency we’ve worked with so far. They understand our product
@@ -26,7 +26,7 @@
         <div class="flex gap-[14px]">
           <img src="../../public/women_pic.svg" alt="" />
           <div>
-            <h6 class="md:mt-4 mt-[2px] md:text-[18px]" style="color: rgba(40, 41, 56, 1)">
+          <h6 class="md:mt-4 mt-[2px] text-[14px] md:text-[18px]" style="color: rgba(40, 41, 56, 1)">
               Jenny Wilson
             </h6>
             <p class="md:mt-4 mt-[2px] text-[10px] md:text-[12px]" style="color: rgba(40, 41, 56, 1)">
