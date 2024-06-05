dfec8d09843c3c1eda5e95004faf67d104db9a58
diyorbekravshanqulov <diyorbekravshanqulov17@gmail.com>
2024-06-05 09:29:52 +0500

    complate

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -29,11 +29,12 @@ const link = (index) => {
 const hamburger = () => {
   const back = document.getElementById("back");
   const hambur = document.getElementById("hambur");
-  const hamMenu = document.getElementById("hamMenu"); // Assuming you have an element with this ID
+  const hamburger = document.getElementById("hamburger");
+  const left = document.getElementById("left");
+  const hamMenu = document.getElementById("hamMenu");
 
   if (back.style.display === "none") {
     hamMenu.style.background = "#1C1E53";
-    // hamMenu.style.background = "darkblue";
     back.style.display = "block";
   } else {
     back.style.display = "none";
@@ -42,45 +43,97 @@ const hamburger = () => {
     hamMenu.style.webkitBackdropFilter = "blur(30px)";
   }
 
-  // Toggle visibility of "hambur" element
-  if (hambur.style.display === "none") {
-    hambur.style.display = "flex";
+  if (hambur.style.right == "-100%") {
+    // hambur.style.display = "flex";
+    hambur.style.right = "0px";
+    hamburger.style.display = "none";
+    left.style.display = "block";
   } else {
-    hambur.style.display = "none";
+    hambur.style.right = "-100%";
+    // hambur.style.display = "none";
+    hamburger.style.display = "block";
+    left.style.display = "none";
   }
 };
 </script>
 
 <template>
-  <div
-    id="hamMenu"
-    class="blurred-background flex px-[3%] md:py-[1%] py-[3%] w-full justify-between items-center fixed z-30"
-  >
-    <a href="#"
-      ><img class="w-[80%]" src="../../public/logo.svg" alt="LOGO"
-    /></a>
-    <div class="flex gap-10 items-center">
-      <router-link
-        :to="link(index)"
-        class="hidden md:block hover-underline-animation font-medium text-[16px] text-white"
-        v-for="(item, index) in menu"
-        :key="index"
-        href="#"
-        >{{ item }}</router-link
-      >
-      <router-link
-        :to="link(5)"
-        class="vibrate-button md:block hidden font-medium text-[16px] text-white md:px-10 md:py-[14px] py-[10px] px-6 rounded-[41px]"
-      >
-        Contact us
-      </router-link>
+  <div class="bg-[#1C1E53] fixed w-full z-40">
+    <div
+      id="hamMenu"
+      class="container max-md:py-5 text-white flex md:py-[1%] w-full justify-between items-center"
+    >
+      <a href="#"><img class="w-[80%]" src="/img/Logo.svg" alt="LOGO" /></a>
+      <div class="flex gap-10 items-center">
+        <router-link
+          :to="link(index)"
+          class="hidden md:block anime font-medium text-[16px] text-white"
+          v-for="(item, index) in menu"
+          :key="index"
+          href="#"
+          >{{ item }}</router-link
+        >
+        <router-link
+          :to="link(5)"
+          class="border-white/20 hidden md:block hover:border-white border-2 py-4 px-12 rounded-full cursor-pointer"
+        >
+          Contact us
+        </router-link>
 
-      <button
-        @click="hamburger()"
-        class="md:hidden flex justify-center items-center text-5xl text-white mb-2 px-4"
-      >
-        &lt;<span class="text-3xl">|</span>
-      </button>
+        <div class="md:hidden">
+          <button
+            id="hamburger"
+            @click="hamburger()"
+            class="flex justify-center items-center text-5xl text-white mb-2 px-4"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1em"
+              height="1em"
+              viewBox="0 0 48 48"
+            >
+              <defs>
+                <mask id="ipSLeftC0">
+                  <g fill="none" stroke-linejoin="round" stroke-width="4">
+                    <path
+                      fill="#fff"
+                      stroke="#fff"
+                      d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z"
+                    />
+                    <path
+                      stroke="#000"
+                      stroke-linecap="round"
+                      d="m27 33l-9-9l9-9"
+                    />
+                  </g>
+                </mask>
+              </defs>
+              <path
+                fill="currentColor"
+                d="M0 0h48v48H0z"
+                mask="url(#ipSLeftC0)"
+              />
+            </svg>
+          </button>
+          <button
+            id="left"
+            @click="hamburger()"
+            class="hidden duration-150 justify-center items-center text-5xl text-white mb-2 px-4"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1em"
+              height="1em"
+              viewBox="0 0 1024 1024"
+            >
+              <path
+                fill="currentColor"
+                d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448s448-200.6 448-448S759.4 64 512 64m154.7 454.5l-246 178c-5.3 3.8-12.7 0-12.7-6.5v-46.9c0-10.2 4.9-19.9 13.2-25.9L566.6 512L421.2 406.8c-8.3-6-13.2-15.6-13.2-25.9V334c0-6.5 7.4-10.3 12.7-6.5l246 178c4.4 3.2 4.4 9.8 0 13"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 
@@ -92,14 +145,8 @@ const hamburger = () => {
 
   <div
     id="hambur"
-    class="w-[65%] hidden bg-blueBox right-0 px-[10%] flex-col gap-6 py-[10%] items-start fixed z-40 shadow-2xl shadow-[#1C1E53]"
+    class="w-[65%] flex -right-full duration-700 bg-[#1C1E53]/70 px-[10%] flex-col gap-6 py-[30%] h-screen items-start fixed z-30 shadow-2xl shadow-[#1C1E53]"
   >
-    <button
-      @click="hamburger()"
-      class="md:hidden flex justify-center items-center text-5xl text-white mb-2"
-    >
-      <span class="text-2xl">|</span>&gt;
-    </button>
     <router-link
       @click="hamburger()"
       :to="link(index)"
@@ -251,5 +298,32 @@ const hamburger = () => {
   100% {
     transform: rotate(0);
   }
+}
+
+.anime {
+  position: relative;
+  color: #bbbbcb;
+  text-decoration: none;
+}
+
+.anime:hover {
+  color: #fff;
+}
+
+.anime::before {
+  content: "";
+  position: absolute;
+  display: block;
+  width: 100%;
+  height: 1px;
+  bottom: 0;
+  left: 0;
+  background-color: #fff;
+  transform: scaleX(0);
+  transition: transform 0.3s ease;
+}
+
+.anime:hover::before {
+  transform: scaleX(1);
 }
 </style>
