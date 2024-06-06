c8481e447ec80d6d7d33f74a60945059643c4d2c
diyorbekravshanqulov <diyorbekravshanqulov17@gmail.com>
2024-06-06 08:55:08 +0500

    complate

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -18,26 +18,26 @@ const func = () => {
 
 <template>
   <div class="temp flex justify-center">
-    <div class="container max-md:px-[5%] md:py-[228px] py-[64px]">
-      <div class="px-[30%] w-full">
+    <div class="container max-md:px-[5%] md:py-[220px] py-[64px] max-md:mt-14">
+      <div class="md:px-[30%] w-full">
         <h2
-          class="text-blueBox text-center font-semibold text-5xl leading-[64px]"
+          class="text-blueBox text-center font-semibold md:text-5xl text-3xl leading-[64px]"
         >
           Contact Us
         </h2>
-        <p class="mt-4 text-blueBox text-center">
+        <p class="mt-4 text-blueBox max-md:text-[14px] text-center">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore.
         </p>
       </div>
 
-      <div class="w-full mt-16 px-[110px]">
+      <div class="w-full mt-16 md:px-[110px]">
         <div
-          class="bg-[#F4F6FC] mx-auto rounded-[12px] py-[55px] px-[77px]"
+          class="bg-[#F4F6FC] mx-auto rounded-[12px] py-[55px] px-4 md:px-[77px]"
         >
           <form
             action=""
-            class="w-full grid grid-cols-2 grid-rows-5 gap-x-8 gap-y-6"
+            class="w-full md:grid grid-cols-2 grid-rows-5 gap-x-8 gap-y-6"
           >
             <div
               v-for="(item, index) in label"
@@ -45,7 +45,7 @@ const func = () => {
               class="flex flex-col"
               :class="index == 4 ? 'col-span-2 row-span-2' : ''"
             >
-              <label class="leading-[32px] text-[18px]" :for="'input-' + index">
+              <label class="md:leading-[32px] md:text-[18px]" :for="'input-' + index">
                 {{ item }}
               </label>
               <input
@@ -54,7 +54,7 @@ const func = () => {
                 type="text"
                 :name="item.toLowerCase()"
                 :placeholder="input[index]"
-                class="border border-gray-300 p-2 rounded-[8px] mt-[6px] px-8 py-[18px] placeholder:text-[#000]"
+                class="border border-gray-300 p-2 text-[14px] rounded-[8px] mt-[6px] px-8 py-[18px] placeholder:text-[#000] max-md:mt-4"
               />
               <textarea
                 name=""
@@ -69,7 +69,7 @@ const func = () => {
             <input
               @click="func()"
               type="submit"
-              class="cursor-pointer duration-300 hover:text-gray-500 border border-transparent hover:bg-white hover:border-gray-500 px-8 w-[254px] py-4 rounded-[41px] bg-blackBlue text-[#F4F6FC] text-[18px] font-semibold mt-8"
+              class="cursor-pointer duration-300 max-md:w-full hover:text-gray-500 border border-transparent hover:bg-white hover:border-gray-500 px-8 w-[254px] py-4 rounded-[41px] bg-blackBlue text-[#F4F6FC] text-[18px] font-semibold mt-8"
             />
           </form>
         </div>
