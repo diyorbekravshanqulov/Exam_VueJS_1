0f1a15dcf42ebe7866b702b5eb94de3367636560
diyorbekravshanqulov <diyorbekravshanqulov17@gmail.com>
2024-06-06 11:08:39 +0500

    complate

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 
 const label = ref(["Name", "Email", "Subject", "Subject", "Message"]);
 const input = ref([
@@ -10,9 +10,24 @@ const input = ref([
   "Write your question here",
 ]);
 
-const func = () => {
-  alert('Message successfully sent!')
+
+const selectInput = (index) => {
+  if (index == 0) {
+    return inputone
+  }else if (index  == 1) {
+    return inputtwo
+  } else if (index == 2) {
+    return inputthree
+  } return inputfour
 }
+
+const func = () => {
+  if (inputone && inputtwo && inputthree && inputfour) {
+    alert("Message successfully sent!");
+  } else {
+    alert("Require!!!")
+  }
+};
 
 </script>
 
@@ -35,7 +50,7 @@ const func = () => {
         <div
           class="bg-[#F4F6FC] mx-auto md:rounded-[12px] py-10 md:py-[55px] px-[5%] md:px-[77px]"
         >
-          <form
+          <form @submt.@click.prevent="handler"
             action=""
             class="w-full md:grid grid-cols-2 grid-rows-5 gap-x-8 gap-y-6"
           >
@@ -45,17 +60,21 @@ const func = () => {
               class="flex flex-col"
               :class="index == 4 ? 'col-span-2 row-span-2' : ''"
             >
-              <label class="md:leading-[32px] md:text-[18px]" :for="'input-' + index">
+              <label
+                class="md:leading-[32px] md:text-[18px]"
+                :for="'input-' + index"
+              >
                 {{ item }}
               </label>
               <input
+                required
                 autocomplete="off"
                 v-if="index != 4"
                 :id="'input-' + index"
-                type="text"
+                :type="index == 1 ? 'email': 'text'"
                 :name="item.toLowerCase()"
                 :placeholder="input[index]"
-                class="border border-gray-300 text-[14px] rounded-[8px] mt-[6px] md:px-8 px-6 md:py-[18px] py-4 placeholder:text-[#000] max-md:mt-4"
+                class="border bg-transparent border-gray-300 text-[14px] rounded-[8px] mt-[6px] md:px-8 px-6 md:py-[18px] py-4 placeholder:text-[#000] max-md:mt-4"
               />
               <textarea
                 name=""
@@ -64,14 +83,14 @@ const func = () => {
                 rows="5"
                 style="resize: none"
                 v-if="index == 4"
-                class="border border-gray-300 p-2 w-full rounded-[8px] mt-[6px] md:px-8 px-6 md:py-[18px] py-4 placeholder:text-[#000]"
+                class="border bg-transparent border-gray-300 p-2 w-full rounded-[8px] mt-[6px] md:px-8 px-6 md:py-[18px] py-4 placeholder:text-[#000]"
               ></textarea>
             </div>
-            <input
+            <button
               @click="func()"
               type="submit"
               class="cursor-pointer duration-300 max-md:w-full hover:text-gray-500 border border-transparent hover:bg-white hover:border-gray-500 px-8 w-[254px] py-4 rounded-[41px] bg-blackBlue text-[#F4F6FC] text-[18px] font-semibold mt-8"
-            />
+            >Send message</button>
           </form>
         </div>
       </div>
