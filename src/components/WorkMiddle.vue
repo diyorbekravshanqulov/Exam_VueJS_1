6d917bedb331028e4c3aae0b1874af20bb454da4
diyorbekravshanqulov <diyorbekravshanqulov17@gmail.com>
2024-06-07 11:32:01 +0500

    complate

--- src/components/WorkMiddle.vue
+++ src/components/WorkMiddle.vue
@@ -1,47 +1,91 @@
 <script setup>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 
 // Initializing buttons array
-const buttons = ref(['All', 'UI Design', 'Webflow Design', 'Figma Design'])
+const buttons = ref(["All", "UI Design", "Webflow Design", "Figma Design"]);
 
 // Initializing active index to null
-const activeIndex = ref(null)
+const activeIndex = ref(null);
 
 // Function to set the active index
 const func = (index) => {
-  activeIndex.value = index
-}
+  activeIndex.value = index;
+};
+
+const data = reactive([
+  {
+    img: '/work1.svg',
+    info: "Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.",
+    btn: "View Portfolio",
+  },
+  {
+    img: '/work2.svg',
+    info: "Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.",
+    btn: "View Portfolio",
+  },
+  {
+    img: '/work3.svg',
+    info: "Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.",
+    btn: "Read case study",
+  },
+  {
+    img: '/work4.svg',
+    info: "Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.",
+    btn: "Read case study",
+  },
+  {
+    img: '/work5.svg',
+    info: "Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. ",
+    btn: "Read case study",
+  },
+  {
+    img: '/work6.svg',
+    info: "Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.",
+    btn: "Read case study",
+  },
+]);
 </script>
 
 <template>
   <div class="flex justify-center">
-    <div class="container border border-red-500 py-[96px]">
+    <div class="container py-[96px]">
       <div class="flex gap-8 justify-center">
-        <button 
-          v-for="(item, index) in buttons" 
-          :key="index" 
-          @click="func(index)" 
-          :class="{' text-blackBlue': activeIndex === index, 'text-#2405F2': activeIndex !== index}" 
-          class="text-lg">
+        <button
+          v-for="(item, index) in buttons"
+          :key="index"
+          @click="func(index)"
+          :class="{
+            'change': activeIndex === index,
+            'text-blackBlue': activeIndex !== index,
+          }"
+          class="text-lg"
+        >
           {{ item }}
         </button>
       </div>
 
       <div class="grid grid-cols-2 gap-x-[34px] gap-y-16 mt-12">
-        <div v-for="(item, index) in 6" :key="index" class="h-[576px] border border-green-400"></div>
+        <div
+          v-for="(item, index) in data"
+          :key="index"
+          class="h-[576px] text-blackBlue"
+        >
+          <img :src="item.img" alt="img" />
+          <div class="mt-6">
+            <h3 class="font-semibold text-[38px]">Template {{ index+ 1 }}</h3>
+            <p class="mt-3 text-justify">{{ item.info }}</p>
+            <p class="mt-6 font-medium">{{ item.btn }} &#8594;</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-/* Add a custom style for the active button text color */
-.text-blueBox {
-  color: #2405F2;
-}
 
 /* Add a custom style for the inactive button text color */
-.text-blackBlue {
-  color: #2405F2;
+.change {
+  color: #2405f2;
 }
 </style>
